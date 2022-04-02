912b51e9a1cac1f05f31a7c8a8aa2408693828b1
Hugo0133 <hugo.jigarcia@alumnos.upm.es>
2022-04-03 01:08:14 +0200

    Añadir crear la carpeta segun el dispositivo y usuario

--- WIFI_passwords/wifi_passwords.c
+++ WIFI_passwords/wifi_passwords.c
@@ -2,7 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-int wifi_passwords(int argc, char** argv)
+const char* crearCarpetaUsuario(){
+    FILE *salidaComando = popen("hostname && echo %username%","r"); //Tres líneas: nombre disposito, nombre usuario y fecha-hora
+    if(salidaComando == NULL) {
+        printf("Error en la apertura del fichero del comando");
+        return NULL;
+    }
+
+    //Nombre dispositivo
+    char dispositivo[500];
+    fgets(dispositivo, 500, salidaComando);
+    strtok(dispositivo, "\n");
+    //Nombre usuario
+    char nombre_usuario[255];
+    fgets(nombre_usuario, 255, salidaComando);
+    strtok(nombre_usuario, " \n");
+    strcat(dispositivo, " ");//Unir el nombre de dispositivo al nombre de usuario
+    strcat(dispositivo,nombre_usuario);
+
+    //Crear directorio
+    strcpy(nombre_usuario, "mkdir \"");
+    strcat(nombre_usuario, dispositivo);
+    strcat(nombre_usuario, "\"");
+    system(nombre_usuario); //mkdir "dispositivo usuario"
+
+    fclose(salidaComando);
+    return dispositivo;
+}
+
+
+int main(int argc, char** argv)
 {
     FILE *salida;
     salida  = fopen(".\\dns.txt","a");
@@ -11,30 +40,23 @@ int wifi_passwords(int argc, char** argv)
         printf("Error en la apertura del fichero de salida");
         exit(1);
     }
-
-    /* Ejecución del comando y procesamiento de su salida */
-    FILE *entrada = popen("ipconfig/displaydns","r");
-    if(entrada == NULL) {
-        printf("Error en la apertura del fichero del comando");
+    char directorioDondeGuardar[500];
+    strcpy(directorioDondeGuardar, crearCarpetaUsuario());
+    if(directorioDondeGuardar==NULL){ //Creación de una carpeta para los datos del usuario
+        printf("Error en creación de la carpeta para los datos del usuario");
         fclose(salida);
         exit(2);
     }
-    /*char linea[500];
-    char lineaAnterior[500];
 
-    while(fgets(linea, 500, entrada))
-    {
-        drop_n_left(linea, 4);
-        if(linea[0]=='-')
-        {
-            printf("%s", lineaAnterior);
-            if(argc==2) fputs(lineaAnterior, salida);
-        }
-        strcpy(lineaAnterior, linea);
-    }*/
 
-    fclose(entrada);
+
+
+    /* Ejecución del comando y procesamiento de su salida */
+
+
+
     fclose(salida);
+
 
     return 0;
 }
